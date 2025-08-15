6edf959ad64a32cdc37d6aaf5a311a7b2ab839ef
nikhil6225 <nikhil6225@gmail.com>
2025-08-15 14:11:08 +0530

    first commit

--- src/test/java/in/psa/TestPsaApplicationTests.java
+++ src/test/java/in/psa/TestPsaApplicationTests.java
@@ -1,13 +1,13 @@
 package in.psa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestPsaApplicationTests {
-
-	@Test
-	void contextLoads() {
+public class TestPsaApplicationTests {
+	public static void main(String[] args) {
+		int x=100;
+		SpringApplication.run(TestPsaApplication.class, args);
 	}
-
-}
+	}
